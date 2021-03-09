2e7adb0b19afc140344958efbdf61773e1ef9ad2
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-03-09 11:08:28 -0600

    Download cypress & npm

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -14,7 +14,7 @@ const UrlContainer = props => {
 
   return (
     <section>
-      { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
+      { urlEls.length ? urlEls : <p>Shorten it up!</p> }
     </section>
   )
 }
